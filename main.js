0d51a8806600d97845c2af22064151e32e0adadf
Nam Wook Kim <namwkim85@gmail.com>
2023-10-30 11:29:01 -0400

    add box-to-select remove polygon select

--- main.js
+++ main.js
@@ -14,6 +14,9 @@ var map = new mapboxgl.Map({
     zoom: 12,
 });
 
+// Disable default box zooming for box-select
+map.boxZoom.disable();
+
 // Load the data and plot the points
 d3.csv("boston-airbnb-listings.csv", d3.autoType).then(function (data) {
     console.log(data);
@@ -34,14 +37,17 @@ d3.csv("boston-airbnb-listings.csv", d3.autoType).then(function (data) {
     var maxPrice = d3.max(data, function (d) {
         return d.price_log_num;
     });
-    
+
     // Add a new source to the map
+    // The source is named "listings" and its type is "geojson".
     map.addSource("listings", {
         type: "geojson",
         data: {
+            //  The data for the source is a FeatureCollection, which is created by mapping over the data array and creating a new Feature object for each item in the array. Each Feature object has a Point geometry with coordinates specified by the longitude and latitude properties of the data item. 
             type: "FeatureCollection",
             features: data.map(function (d) {
                 return {
+                    //The Feature object also has a properties object with several properties including listing_url, name, price, price_log_num, and rating. These properties are used to create the heatmap layer and the circle layer that is added to the map in the code below
                     type: "Feature",
                     geometry: {
                         type: "Point",
@@ -123,10 +129,13 @@ d3.csv("boston-airbnb-listings.csv", d3.autoType).then(function (data) {
 
     // Add a new layer to the map
     map.addLayer({
+        // The layer is defined with an ID of "listings-circles" and a type of "circle". The source of the layer is set to "listings".
         id: "listings-circles",
         type: "circle",
         source: "listings",
+        // The paint property is used to define the visual appearance of the circles. 
         paint: {
+            //The circle-radius property is set using a stops array that maps the "rating" property of each circle to a specific radius value. The minRating and maxRating variables are used to define the range of possible rating values.
             "circle-radius": {
                 property: "rating",
                 stops: [
@@ -134,6 +143,8 @@ d3.csv("boston-airbnb-listings.csv", d3.autoType).then(function (data) {
                     [maxRating, 10],
                 ],
             },
+            // The circle-color property is set using an "interpolate" expression that maps the "price_log_num" property of each circle to a specific color value. The minPrice and maxPrice variables are used to define the range of possible price values. The color values are defined using hexadecimal color codes.
+
             "circle-color": [
                 "interpolate",
                 ["linear"],
@@ -143,11 +154,13 @@ d3.csv("boston-airbnb-listings.csv", d3.autoType).then(function (data) {
                 maxPrice,
                 "#7f0000",
             ],
+            // Finally, the circle-opacity property is set to 0.8 to define the opacity of the circles.  
             "circle-opacity": 0.8,
         },
     });
-    
+
     // Add a new layer to the map
+    // This layer is used to highlight the selected circles by showing boundaries around them. See drag-to-select section below.
     map.addLayer({
         id: "listings-circles-highlighted",
         type: "circle",
@@ -170,7 +183,7 @@ d3.csv("boston-airbnb-listings.csv", d3.autoType).then(function (data) {
 
 
 
-    // Add tooltip to the circles
+    // ***** Add tooltip to the circles (start) *****//
     let tooltip;
     map.on("mouseenter", "listings-circles", function (e) {
         var features = map.queryRenderedFeatures(e.point, {
@@ -189,12 +202,12 @@ d3.csv("boston-airbnb-listings.csv", d3.autoType).then(function (data) {
             .setLngLat(feature.geometry.coordinates)
             .setHTML(
                 "<h3>" +
-                    feature.properties.name +
-                    "</h3><p>Price: $" +
-                    feature.properties.price +
-                    "</p><p>Rating: " +
-                    feature.properties.rating +
-                    "</p>"
+                feature.properties.name +
+                "</h3><p>Price: $" +
+                feature.properties.price +
+                "</p><p>Rating: " +
+                feature.properties.rating +
+                "</p>"
             )
             .addTo(map);
     });
@@ -204,88 +217,115 @@ d3.csv("boston-airbnb-listings.csv", d3.autoType).then(function (data) {
         map.getCanvas().style.cursor = "";
         tooltip.remove();
     });
+    // ***** Add tooltip to the circles (end) *****//
+
+    map.addControl(new mapboxgl.NavigationControl()); // Add zoom and rotation controls to the map.
 
     map.setLayoutProperty("listings-heatmap", "visibility", "none");
     map.setLayoutProperty("listings-circles", "visibility", "visible");
     map.setLayoutProperty("listings-circles-highlighted", "visibility", "visible");
 
-    // Initialize the draw control
-    // reference: https://github.com/charliedotau/mapbox-gl-js-select-features-by-draw/blob/master/index.html
+    //******************* drag to select (start) ****************/
     // reference: https://docs.mapbox.com/mapbox-gl-js/example/using-box-queryrenderedfeatures/
-    var draw = new MapboxDraw({
-        displayControlsDefault: false,
-        controls: {
-            polygon: true,
-            trash: true,
-        },
-    });
-    map.addControl(new mapboxgl.NavigationControl());
-    map.addControl(draw);
+    const canvas = map.getCanvasContainer();
 
-    // When a new circle is drawn, filter the listings-circles layer to show only the selected circles
-    map.on("draw.create", select);
-    map.on("draw.update",select );
-    function select(e) {
-        
-        // Convert the features to a bounding box
-        var userPolygon = e.features[0];
+    // Set `true` to dispatch the event before other functions
+    // call it. This is necessary for disabling the default map
+    // dragging behaviour.
+    canvas.addEventListener('mousedown', mouseDown, true);
 
-        // generate bounding box from polygon the user drew
-        var polygonBoundingBox = turf.bbox(userPolygon);
-
-        var southWest = [polygonBoundingBox[0], polygonBoundingBox[1]];
-        var northEast = [polygonBoundingBox[2], polygonBoundingBox[3]];
-
-        var northEastPointPixel = map.project(northEast);
-        var southWestPointPixel = map.project(southWest);
-
-        var selectedCircles = [];
-
-        var features = map.queryRenderedFeatures(
-            [southWestPointPixel, northEastPointPixel],
-            {
-                layers: ["listings-circles"],
-            }
+    // Return the xy coordinates of the mouse position
+    function mousePos(e) {
+        const rect = canvas.getBoundingClientRect();
+        return new mapboxgl.Point(
+            e.clientX - rect.left - canvas.clientLeft,
+            e.clientY - rect.top - canvas.clientTop
         );
-        
-        var selectedCircles = features.reduce(function (memo, feature) {
-            // console.log(JSON.parse(JSON.stringify(feature)), userPolygon)
-            if (!(undefined === turf.inside(feature, userPolygon))) {
-                // only add the property, if the feature intersects with the polygon drawn by the user
-
-                memo.push(feature.properties.listing_url);
-            }
-
-            return memo;
-        },[]);
-        console.log(selectedCircles.length);
-        console.log(["in", "listing_url"].concat(selectedCircles))
-        // features.forEach(function (feature) {
-        //     selectedCircles.push(feature.properties.name);
-        // });
-        
-        map.setFilter(
-            "listings-circles-highlighted",["in", "listing_url"].concat(selectedCircles)
-        );
-        map.dragPan.disable();
     }
-    // When a circle is removed, reset the filter to show all circles
-    map.on("draw.delete", function () {
-        map.setFilter("listings-circles-highlighted", ["in", "listing_url", ""]);
-    });
+    let start, current, box;
+    function mouseDown(e) {
+        // Continue the rest of the function if the shiftkey is pressed.
+        if (!(e.shiftKey && e.button === 0)) return;
 
-    // When the draw mode is exited, reset the filter to show all circles
-    map.on("draw.modechange", function () {
-        map.setFilter("listings-circles-highlighted", ["in", "listing_url", ""]);
-    });
+        // Disable default drag zooming when the shift key is held down.
+        map.dragPan.disable();
 
-    // Mute out unselected circles
-    map.setPaintProperty("listings-circles", "circle-opacity", [
-        "case",
-        ["boolean", ["feature-state", "selected"], false],
-        0.7,
-        0.2,
-    ]);
+        // Call functions for the following events
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mouseup', onMouseUp);
+        document.addEventListener('keydown', onKeyDown);
+
+        // Capture the first xy coordinates
+        start = mousePos(e);
+    }
+
+    function onMouseMove(e) {
+        // Capture the ongoing xy coordinates
+        current = mousePos(e);
+
+        // Append the box element if it doesnt exist
+        if (!box) {
+            box = document.createElement('div');
+            box.classList.add('boxdraw');
+            canvas.appendChild(box);
+        }
+
+        const minX = Math.min(start.x, current.x),
+            maxX = Math.max(start.x, current.x),
+            minY = Math.min(start.y, current.y),
+            maxY = Math.max(start.y, current.y);
+
+        // Adjust width and xy position of the box element ongoing
+        const pos = `translate(${minX}px, ${minY}px)`;
+        box.style.transform = pos;
+        box.style.width = maxX - minX + 'px';
+        box.style.height = maxY - minY + 'px';
+    }
+
+    function onMouseUp(e) {
+        // Capture xy coordinates
+        finish([start, mousePos(e)]);
+    }
+
+    function onKeyDown(e) {
+        // If the ESC key is pressed
+        if (e.keyCode === 27) finish();
+    }
+
+    function finish(bbox) {
+        // Remove these events now that finish has been called.
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('keydown', onKeyDown);
+        document.removeEventListener('mouseup', onMouseUp);
+
+        if (box) {
+            box.parentNode.removeChild(box);
+            box = null;
+        }
+
+        // If bbox exists. use this value as the argument for `queryRenderedFeatures`
+        if (bbox) {
+            const features = map.queryRenderedFeatures(bbox, {
+                layers: ['listings-circles']
+            });
+
+            if (features.length >= 1000) {
+                return window.alert('Select a smaller number of features');
+            }
+
+            let selectedCircles = [];
+            features.forEach(function (feature) {
+                selectedCircles.push(feature.properties.listing_url);
+            });
+
+            map.setFilter(
+                "listings-circles-highlighted", ["in", "listing_url"].concat(selectedCircles)
+            );
+        }
+
+        map.dragPan.enable();
+    }
+    //******************* drag to select (end) ****************/
 });
 
 // Define a variable to keep track of the current layer
@@ -293,7 +333,7 @@ let currentLayer = "listings-circles";
 
 // Add a toggle button to switch between the heatmap and the circles
 const toggleButton = document.querySelector("#button");
-toggleButton.textContent = "Toggle Layer";
+// toggleButton.textContent = "Toggle Layer";
 toggleButton.addEventListener("click", function () {
     if (currentLayer === "listings-heatmap") {
         map.setLayoutProperty("listings-heatmap", "visibility", "none");
